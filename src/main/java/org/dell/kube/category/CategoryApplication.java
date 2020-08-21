4b3afeefb328bbb7ff4e5aeb1d18551fc925520f
DJ <deepakj6@yahoo.co.in>
2020-08-21 23:15:02 +0530

    Distributed-Solution

--- src/main/java/org/dell/kube/category/CategoryApplication.java
+++ src/main/java/org/dell/kube/category/CategoryApplication.java
@@ -2,8 +2,10 @@ package org.dell.kube.category;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CategoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(CategoryApplication.class,args);
